0de1748baa225849e3495b12e4a5208062b689a5
ankitcharpe01 <ankitcharpe01@gmail.com>
2025-02-22 11:31:13 +0530

    [Ankit Charpe]Add.Make REST Call to show Hello

--- SprigMessagingApp/src/main/java/com/example/SprigMessagingApp/Controller/SpringMessagingApplication.java
+++ SprigMessagingApp/src/main/java/com/example/SprigMessagingApp/Controller/SpringMessagingApplication.java
@@ -3,18 +3,23 @@ package com.example.SprigMessagingApp.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@RestController
+public class SpringMessagingApplication {
 
-    @RestController
-    public class SpringMessagingApplication {
-
-        @GetMapping("/hello")
-        public String sayHello() {
-            return "Hello From Bridgelabz";
-        }
-
-        @GetMapping("/hello/param/{name}")
-        public String sayHello(@PathVariable String name) {
-            return "Hello " + name + " from BridgeLabz";
-        }
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello From Bridgelabz";
     }
+
+    @GetMapping("/hello/query")
+    public String getMapping(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+
+    @GetMapping("/hello/param/{name}")
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from BridgeLabz";
+    }
+}
